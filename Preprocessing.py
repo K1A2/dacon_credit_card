9c24f737b3f84a7211a8c9677e7b15bfeaa98682
K1A2 <jckim0307@gmail.com>
2021-11-18 13:47:38 +0900

    change encoder

--- Preprocessing.py
+++ Preprocessing.py
@@ -438,7 +438,7 @@ class PreprocesserGBoost():
                     e = pickle.load(f)
             else:
                 with open(default_path + f'encoder/label_encoding_{column}', 'wb') as f:
-                    e.fit(label_all_data[column].values, y)
+                    e.fit(df[column].values, y)
                     pickle.dump(e, f)
             df[column] = e.transform(df[column].values)
 
